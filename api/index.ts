682225f26634385de4533820efc15f6b3dd5eaae
Alex <alexcroox@users.noreply.github.com>
2023-01-25 22:48:42 +0000

    Delete index.ts

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import { Client, Intents } from 'discord.js'
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-const list = client.guilds.cache.get('303681520202285057')
-list.members.cache.forEach((member) => console.log(member.user.username))
-client.login('myTokenID')
