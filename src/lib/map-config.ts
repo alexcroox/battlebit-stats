70267ba4e4cbbf17dc6493881ad6e54e92bb9e96
Cade Cuddy <53287407+cadecuddy@users.noreply.github.com>
2023-01-26 03:53:32 -0500

    Bring weapon & map materials up-to-date (#1)
    * Add azagor
    
    * Add Isle
    
    * Add Lonovo
    
    * Add updated weapon .obj files
    
    * Update ModelViewer to reflect new obj axis
    
    * Add new maps to map-config & update known issues
    
    * Add missing weapons
    
    * Order maps by popularity
    
    * Add Basra/Frugis/Oil-Dunes/River main map & thumbs
    
    * Make prestige stat dynamic
    
    Co-authored-by: Cade Cuddy <cadecuddy@users.noreply.github.com>

--- src/lib/map-config.ts
+++ src/lib/map-config.ts
@@ -6,14 +6,14 @@ export interface MapConfig {
 }
 
 export const maps: Record<string, MapConfig> = {
-  'multu-islands': {
-    bgColor: '#535264',
-    name: 'Multu Islands',
+  isle: {
+    bgColor: '#82969F',
+    name: 'Isle',
     description: 'Awaiting description'
   },
-  district: {
-    bgColor: '#363D2D',
-    name: 'District',
+  basra: {
+    bgColor: '#7292a6',
+    name: 'Basra',
     description: 'Awaiting description'
   },
   valley: {
@@ -21,9 +21,9 @@ export const maps: Record<string, MapConfig> = {
     name: 'Valley',
     description: 'Awaiting description'
   },
-  eduardovo: {
-    bgColor: '#3B4633',
-    name: 'Eduardovo',
+  'sandy-sunset': {
+    bgColor: '#864A28',
+    name: 'Sandy Sunset',
     description: 'Awaiting description'
   },
   salhan: {
@@ -33,19 +33,9 @@ export const maps: Record<string, MapConfig> = {
     description:
       'Salhan is an urban-desert map sized at 2x2 km. The origins of the map comes from Iraq. It is more focused on close/medium range combat.'
   },
-  'dusty-dew': {
-    bgColor: '#3F3023',
-    name: 'Dusty Dew',
-    description: 'Awaiting description'
-  },
-  'sandy-sunset': {
-    bgColor: '#864A28',
-    name: 'Sandy Sunset',
-    description: 'Awaiting description'
-  },
-  'wine-paradise': {
-    bgColor: '#613A1E',
-    name: 'Wine Paradise',
+  district: {
+    bgColor: '#363D2D',
+    name: 'District',
     description: 'Awaiting description'
   },
   wakistan: {
@@ -55,19 +45,59 @@ export const maps: Record<string, MapConfig> = {
     description:
       'Wakistan is a forest map at 2x2 km. The origins of the map come from the mountain of Tebulosmta in Georgia. It has picturesque mountains, forests, and rivers. Its topography gives players who seize hills an advantage over their enemies.'
   },
+  lonovo: {
+    bgColor: '#484D37',
+    name: 'Lonovo',
+    description: 'Awaiting description'
+  },
+  'oil-dunes': {
+    bgColor: '#8b5930',
+    name: 'Oil Dunes',
+    description: 'Awaiting description'
+  },
   construction: {
     bgColor: '#653F3D',
     name: 'Construction',
     description: 'Awaiting description'
   },
+  eduardovo: {
+    bgColor: '#3B4633',
+    name: 'Eduardovo',
+    description: 'Awaiting description'
+  },
+  'dusty-dew': {
+    bgColor: '#3F3023',
+    name: 'Dusty Dew',
+    description: 'Awaiting description'
+  },
+  frugis: {
+    bgColor: '#58573b',
+    name: 'Frugis',
+    description: 'Awaiting description'
+  },
+  azagor: {
+    bgColor: '#83633C',
+    name: 'Azagor',
+    description: 'Awaiting description'
+  },
+  river: {
+    bgColor: '#793922',
+    name: 'River',
+    description: 'Awaiting description'
+  },
+  'wine-paradise': {
+    bgColor: '#613A1E',
+    name: 'Wine Paradise',
+    description: 'Awaiting description'
+  },
+  'multu-islands': {
+    bgColor: '#535264',
+    name: 'Multu Islands',
+    description: 'Awaiting description'
+  },
   range: {
     bgColor: '#9BBAAE',
     name: 'Range',
-    description: 'Awaiting description'
-  },
-  'hot-land': {
-    bgColor: '#86542D',
-    name: 'Hot Land',
     description: 'Awaiting description'
   }
 }
