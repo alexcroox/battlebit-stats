80b16de3a14614948dfe3bfdbebdcfbb258a521d
Alex Crooks <alexcrooks@gmail.com>
2023-09-24 15:02:28 +0100

    fix: handle 404 correctly

---
+++ src/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const router = useRouter()
+</script>
+
+<template>
+  <NuxtLayout>
+    <main class="flex flex-col items-center justify-center h-full">
+      <div class="px-8 py-6 text-center bg-gray-700 rounded">
+        <h1 class="text-2xl">{{ $t('pageNotFound') }}</h1>
+
+        <Button class="w-full mt-6" to="/">
+          {{ $t('returnToCombatArea') }}
+        </Button>
+      </div>
+    </main>
+  </NuxtLayout>
+</template>
--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,13 +1,3 @@
-<template>
-  <div class="flex flex-col items-center justify-center h-full">
-    <div class="px-8 py-6 text-center bg-gray-700 rounded">
-      <h1 class="text-2xl">{{ $t('pageNotFound') }}</h1>
-
-      <router-view />
-
-      <Button class="w-full mt-6" to="/">
-        {{ $t('returnToCombatArea') }}
-      </Button>
-    </div>
-  </div>
-</template>
+<script setup lang="ts">
+throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
+</script>
